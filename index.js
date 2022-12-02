dccf9baa8a9d967994d793389dc1868011651c3b
lizafaria <farializa24@gmail.com>
2022-12-02 15:28:52 +0600

    third commit

--- index.js
+++ index.js
@@ -18,3 +18,34 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("Access Denied");
+  }
+  const token = authHeader.split("")[1];
+  jwt.verify(token, process.env.DB_AccessToken, function (error, decoded) {
+    if (error) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+async function run() {
+  try {
+    const categoryCollection = client
+      .db("Bike-Mart")
+      .collection("allbikedetails");
+    const bikeCollection = client.db("Bike-Mart").collection("allbikecategory");
+    const wishCollection = client.db("Bike-Mart").collection("Wishlist");
+    const reportCollection = client.db("Bike-Mart").collection("report");
+    const adCollection = client.db("Bike-Mart").collection("Ad");
+
+    const orderCollection = client.db("Bike-Mart").collection("orders");
+    const usersCollection = client.db("Bike-Mart").collection("users");
+    const sellersCollection = client.db("Bike-Mart").collection("sellers");
+    const paymentsCollection = client.db("Bike-Mart").collection("payments");
+
+  }}
