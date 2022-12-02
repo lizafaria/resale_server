612a6d42203aa7d965d627b9e63b748bad646342
lizafaria <farializa24@gmail.com>
2022-12-02 15:40:21 +0600

    added commit

--- index.js
+++ index.js
@@ -206,4 +206,23 @@ async function run() {
     res.status(403).send({ accessToken: "Restricted Access" });
   });
 
+  // Users....
+  app.post("/allusers", async (req, res) => {
+    const user = req.body;
+    const result = await usersCollection.insertOne(user);
+    res.send(result);
+  });
+
+  app.get("/users/admin/:email", async (req, res) => {
+    const email = req.params.email;
+    const query = { email };
+    const user = await usersCollection.findOne(query);
+    res.send({ isAdmin: user?.role === "admin" });
+  });
+  app.get("/users/seller/:email", async (req, res) => {
+    const email = req.params.email;
+    const query = { email };
+    const user = await usersCollection.findOne(query);
+    res.send({ isSeller: user?.role === "seller" });
+  });
   
